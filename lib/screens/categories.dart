529e414f129e08f6209f8fea76a58f594d175f7f
Ahsanwarsi <ahsan.ue@gmail.com>
2024-03-31 09:51:26 +0500

    Deleted Unnecessary Files

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
-import 'package:meals_app/models/category.dart';
 import 'package:meals_app/widgets/category.grid.item.dart';
 
 class CategoriesScreen extends StatelessWidget{
